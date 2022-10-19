33ea335965dacee74b7e317806028af53e1ffe61
sonke <inwoker123@mail.ru>
2022-10-19 17:57:59 +0300

    commit new test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnDataTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Test data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Test data"))
+        ;
+    }
+}
